a7a4939115e0e1dc7d42070a2ec016b43ffe5957
Anton Johansson <antoon.johansson@gmail.com>
2016-12-03 17:25:09 +0100

    Add Eclipse installation script

---
+++ tools/eclipse/README.md
@@ -0,0 +1,8 @@
+# Eclipse
+
+Installs Eclipse with a given generation and version.
+
+
+## Usage
+
+wget https://raw.githubusercontent.com/anton-johansson/linux-configs/master/tools/eclipse/eclipse.sh -O - | sudo bash -s neon 1a
---
+++ tools/eclipse/eclipse.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# This script installs Eclipse.
+# Usage: sudo ./eclipse.sh neon 1a
+
+
+###############################################
+# Check input arguments
+###############################################
+
+if [ "$(whoami)" != "root" ]; then
+    echo "Sorry, you are not root."
+    exit 1
+fi
+
+if [ ! -z "$1" ]; then
+    generation="$1"
+else
+    echo "Invalid generation"
+    exit 1
+fi
+
+if [ ! -z "$2" ]; then
+    version="$2"
+else
+    echo "Invalid version"
+    exit 1
+fi
+
+###############################################
+# Download Eclipse
+###############################################
+
+echo "Downloading Eclipse $generation $version..."
+sudo wget -O /opt/eclipse.tar.gz http://saimei.acc.umu.se/mirror/eclipse.org/technology/epp/downloads/release/$generation/$version/eclipse-java-$generation-$version-linux-gtk-x86_64.tar.gz
+
+if [ $? -ne 0 ]; then
+    echo "Could not find Eclipse version"
+fi
+
+
+###############################################
+# Extract Eclipse
+###############################################
+
+sudo tar -zxf /opt/eclipse.tar.gz -C /opt/
+sudo rm /opt/eclipse.tar.gz
+sudo ln -s /opt/eclipse/eclipse /usr/local/bin/eclipse
+
+
+###############################################
+# Create launcher
+###############################################
+
+if [ -d "$HOME/.local/share/applications" ]; then
+    echo "[Desktop Entry]" > $HOME/.local/share/applications/eclipse.desktop
+    echo "Type=Application" >> $HOME/.local/share/applications/eclipse.desktop
+    echo "Name=Eclipse" >> $HOME/.local/share/applications/eclipse.desktop
+    echo "Comment=Eclipse Integrated Development Environment" >> $HOME/.local/share/applications/eclipse.desktop
+    echo "Icon=/opt/eclipse/icon.xpm" >> $HOME/.local/share/applications/eclipse.desktop
+    echo "Exec=/opt/eclipse/eclipse" >> $HOME/.local/share/applications/eclipse.desktop
+    echo "Categories=Development;IDE;Java;" >> $HOME/.local/share/applications/eclipse.desktop
+fi
