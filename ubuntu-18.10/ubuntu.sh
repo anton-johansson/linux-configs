e26a43c9daed15603c52e37d7287c5f93890e557
Anton Johansson <antoon.johansson@gmail.com>
2018-10-30 16:02:21 +0100

    Add script for Cosmic Cuttlefish

---
+++ ubuntu-18.10/ubuntu.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# This script configures a Ubuntu 18.10 (Cosmic Cuttlefish) distribution.
+
+# Repository for Google Chrome
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# Repository for Oracle Java 8
+sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/oracle-java.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+
+# Repository for Spotify
+sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+
+# Update repository
+sudo apt update
+
+# Install common software
+sudo apt --yes --force-yes install zsh subversion git meld google-chrome-stable oracle-java8-installer maven colordiff openssh-server spotify-client
+
+# Fix Eclipse web components (such as JavaDocs)
+#sudo apt --yes --force-yes install libwebkitgtk-3.0-0
+
+# Add user to the docker group
+#sudo gpasswd -a $USER docker
+
+# Install Eclipse
+cd /opt
+sudo wget http://saimei.acc.umu.se/mirror/eclipse.org/technology/epp/downloads/release/2018-09/R/eclipse-java-2018-09-linux-gtk-x86_64.tar.gz
+sudo tar -zxvf eclipse-java-2018-09-linux-gtk-x86_64.tar.gz
+sudo ln -s /opt/eclipse/eclipse /usr/local/bin/eclipse
+mkdir -p ~/.local/share/applications
+cd ~/.local/share/applications
+echo "[Desktop Entry]" > eclipse.desktop
+echo "Type=Application" >> eclipse.desktop
+echo "Name=Eclipse" >> eclipse.desktop
+echo "Comment=Eclipse Integrated Development Environment" >> eclipse.desktop
+echo "Icon=/opt/eclipse/icon.xpm" >> eclipse.desktop
+echo "Exec=/opt/eclipse/eclipse" >> eclipse.desktop
+echo "Categories=Development;IDE;Java;" >> eclipse.desktop
+
+# Install Postman
+cd /opt
+sudo wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo tar -zxvf postman.tar.gz
+cd ~/.local/share/applications
+echo "[Desktop Entry]" > postman.desktop
+echo "Type=Application" >> postman.desktop
+echo "Name=Postman" >> postman.desktop
+echo "Comment=Postman" >> postman.desktop
+echo "Icon=/opt/Postman/app/resources/app/assets/icon.png" >> postman.desktop
+echo "Exec=/opt/Postman/app/Postman" >> postman.desktop
+echo "Categories=HTTP;REST;Client;" >> postman.desktop
+
+# Install Microsoft SQL Operations Studio
+#cd /opt
+#sudo wget https://go.microsoft.com/fwlink/?linkid=862646 -O sql-operations-studio.tar.gz
+#sudo tar -zxvf sql-operations-studio.tar.gz
+#cd ~/.local/share/applications
+#echo "[Desktop Entry]" > sql-operations-studio.desktop
+#echo "Type=Application" >> sql-operations-studio.desktop
+#echo "Name=SQL Operations Studio" >> sql-operations-studio.desktop
+#echo "Comment=Microsoft SQL Operations Studio" >> sql-operations-studio.desktop
+#echo "Icon=/opt/sqlops-linux-x64/resources/app/resources/linux/code.png" >> sql-operations-studio.desktop
+#echo "Exec=/opt/sqlops-linux-x64/sqlops" >> sql-operations-studio.desktop
+#echo "Categories=Development;IDE;SQL;" >> sql-operations-studio.desktop
+#sudo rm /opt/sql-operations-studio.tar.gz
